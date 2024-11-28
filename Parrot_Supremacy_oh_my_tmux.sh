f48cf66f6a5564124c9ea09f992a299c31ff5009
Afonso Goncalves Mota <afogonca@c1r5s6.42porto.com>
2024-11-28 12:16:38 +0000

    Added versions for oh-my-tmux and original tmux

--- Parrot_Supremacy_oh_my_tmux.sh
+++ Parrot_Supremacy_oh_my_tmux.sh
@@ -7,7 +7,7 @@ if (env | grep TMUX); then
 	tmux last-pane
 	tmux split-window -p 50 -h
 	
-	tmux select-pane -t 0
+	tmux select-pane -t 1
 	
 	# Second split horizontal
 	
@@ -18,7 +18,7 @@ if (env | grep TMUX); then
 	tmux last-pane
 	tmux split-window -p 50 -h
 	
-	tmux select-pane -t 0
+	tmux select-pane -t 1
 	
 	# Last split horizontal
 	tmux split-window -p 25 -h
@@ -27,12 +27,11 @@ if (env | grep TMUX); then
 	tmux last-pane
 	tmux split-window -p 50 -h
 	
-	tmux select-pane -t 0
+	tmux select-pane -t 1
 	
 	#	PARROT PART YIPPPIEEE
 	
 	
-	tmux send-keys -t 1 'curl parrot.live' Enter
 	tmux send-keys -t 2 'curl parrot.live' Enter
 	tmux send-keys -t 3 'curl parrot.live' Enter
 	tmux send-keys -t 4 'curl parrot.live' Enter
@@ -43,10 +42,11 @@ if (env | grep TMUX); then
 	tmux send-keys -t 9 'curl parrot.live' Enter
 	tmux send-keys -t 10 'curl parrot.live' Enter
 	tmux send-keys -t 11 'curl parrot.live' Enter
+	tmux send-keys -t 12 'curl parrot.live' Enter
 	curl parrot.live
 else
 	tmux new -d -s PARROT
 	sleep 2
-	tmux send-keys -t PARROT.0 './Parrot_Supremacy.sh' ENTER
+	tmux send-keys -t PARROT.1 './Parrot_Supremacy.sh' ENTER
 	tmux a -t PARROT
 fi
